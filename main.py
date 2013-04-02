21f9f5e4db40f7da387cc4d843070db0ea2a3131
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-04-01 20:40:54 -0700

    Adding complete main file

--- main.py
+++ main.py
@@ -3,8 +3,17 @@
 import sys
 
 
+from moonlander import Moonlander
+
+
 def main():
-    pass
+    the_moonlander = Moonlander()
+    while the_moonlander.test() == 'in_air':
+        the_moonlander.update()
+        the_moonlander.output()
+    print('We are no longer in the air... Final stats to follow.')
+    the_moonlander.output()
+    return
 
 
 if __name__ == '__main__':
