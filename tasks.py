e522a7045d075addb2be8870219b641a0cbcb7fe
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-04-27 18:44:06 -0700

    Updating task to add own environment; adding normalization

--- tasks.py
+++ tasks.py
@@ -1,23 +1,27 @@
 from pybrain.rl.environments import EpisodicTask
+from environment import Lander
 
 
 class LanderTask(EpisodicTask):
     """
     Task that represents lander attempting to land.
     """
-    defaultReward = 0
-    crashedPenalty = -1
-    landed = 1
 
-    def __init__(self, environment):
+    def __init__(self, environment=None):
+        if environment is None:
+            environment = Lander()
         EpisodicTask.__init__(self, environment)
-        self.reset()
+
+        self.defaultReward = 0
+        self.crashedPenalty = -1
+        self.landed = 1
+
+        self.sensor_limits = [(0, 100), (1, 30), (1, 3),
+                              (-1, 1), (-3, 3), (-1, 1), (0, 100)]
+        self.actor_limits = [(0, 9), (-3, 3)]
 
     def isFinished(self):
-        status = self.env.status == 'landed' or self.env.status == 'crashed'
-        if status:
-            self.env.printResults()
-        return status
+        return self.env.status == 'landed' or self.env.status == 'crashed'
 
     def getReward(self):
         if self.env.status == 'in_air':
