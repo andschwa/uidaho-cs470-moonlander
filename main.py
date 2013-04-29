06ad50e5a7ee241dccc9efc04f13e2a71e59e1e4
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-04-29 01:44:39 -0700

    Adding fitness calculations from HillClimber

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import sys
 
+from pybrain.utilities import fListToString
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.optimization import HillClimber
 from pybrain.rl.agents import OptimizationAgent
@@ -13,9 +14,14 @@ from tasks import LanderTask
 def main():
     task = LanderTask()
     net = buildNetwork(task.indim, 5, task.outdim)
-    agent = OptimizationAgent(net, HillClimber())
+    learner = HillClimber(storeAllEvaluations=True)
+    agent = OptimizationAgent(net, learner)
     experiment = EpisodicExperiment(task, agent)
     experiment.doEpisodes(100)
+    print('Episodes learned from {}'.format(len(learner._allEvaluations)))
+    n, fit = learner._bestFound()
+    print('Best fitness found: {}, with this network: {}'.format(fit, n))
+    print('Containing these parameters: {}'.format(fListToString(n.params, 7)))
 
 
 if __name__ == '__main__':
