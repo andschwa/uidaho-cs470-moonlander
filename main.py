3c2a2bce68b0911c3f9398038cd363e3ec85499c
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-04-30 14:40:54 -0700

    Adding notes

--- main.py
+++ main.py
@@ -12,9 +12,23 @@ from tasks import LanderTask
 
 
 def main():
+    """
+    The task represents one full simulation. Therefore it is episodic.
+    Each episode calls performAction after passing getObservation to the agent.
+    Once isFinished is true, the reward is returned and one simulation is done.
+
+    The net is the neural network. It has 7 input nodes, a hidden layer of 5
+    nodes, and 2 output nodes. It is a feed-forward network using sigmoid
+    activation functions.
+
+    OptimizationAgent(module, learner)
+    EpisodicExperiment.optimizer = learner
+    learner.setEvaluator(task, module)
+    optimizer.learn()
+    """
     task = LanderTask()
     net = buildNetwork(task.indim, 5, task.outdim)
-    learner = HillClimber(storeAllEvaluations=True)
+    learner = HillClimber(storeAllEvaluations=True, verbose=True)
     agent = OptimizationAgent(net, learner)
     experiment = EpisodicExperiment(task, agent)
     experiment.doEpisodes(100)
