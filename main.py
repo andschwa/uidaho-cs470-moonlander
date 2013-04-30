8ea00719d96fe7744db5a7a478f16b3dbc45d8a8
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-04-30 16:07:16 -0700

    Updating main

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 import sys
 
-from pybrain.utilities import fListToString
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.optimization import HillClimber
 from pybrain.rl.agents import OptimizationAgent
@@ -28,14 +27,10 @@ def main():
     """
     task = LanderTask()
     net = buildNetwork(task.indim, 5, task.outdim)
-    learner = HillClimber(storeAllEvaluations=True, verbose=True)
+    learner = HillClimber()
     agent = OptimizationAgent(net, learner)
     experiment = EpisodicExperiment(task, agent)
-    experiment.doEpisodes(100)
-    print('Episodes learned from {}'.format(len(learner._allEvaluations)))
-    n, fit = learner._bestFound()
-    print('Best fitness found: {}, with this network: {}'.format(fit, n))
-    print('Containing these parameters: {}'.format(fListToString(n.params, 7)))
+    experiment.doEpisodes(1000)
 
 
 if __name__ == '__main__':
