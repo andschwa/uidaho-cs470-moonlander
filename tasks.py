f3cff0b345573f6aa5ba0807ad0efea9f6ba0211
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-05-04 13:03:10 -0700

    Crashing when out of fuel
    Because you're dead anyway if you've used all your fuel
    Also printing reward and adjusting values

--- tasks.py
+++ tasks.py
@@ -21,10 +21,17 @@ class LanderTask(EpisodicTask):
         self.actor_limits = [(0.0, 10.0), (-1.0, 1.0)]
 
     def isFinished(self):
-        return self.env.status == 'landed' or self.env.status == 'crashed'
+        return (self.env.status == 'landed' or
+                self.env.status == 'crashed' or
+                self.env.fuel == 0.)
+        # If you run out of fuel you're dead anyway
 
     def getReward(self):
         reward = 0
+        if self.env.status == 'in_air':
+            used_fuel = 100 - self.env.fuel
+            if used_fuel > 60:
+                reward -= used_fuel*used_fuel*used_fuel
         if self.env.status == 'crashed':
             if self.env.y_velocity > self.env.max_safe_landing_speed:
                 error = ceil(abs(self.env.y_velocity) /
@@ -34,7 +41,7 @@ class LanderTask(EpisodicTask):
                 error = ceil(abs(self.env.x_position) /
                                self.env.max_safe_x)
                 reward -= error*error
-            print('Returning full crashed reward: {}'.format(reward))
         if self.env.status == 'landed':
-            reward += 5000
+            reward += 100000 
+        print('Reward: {}'.format(reward))
         return reward
